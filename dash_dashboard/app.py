b4474c6d7519a86a05b7f4ae37a0af7c9f3730d9
raviolli77 <rauleulogio3@gmail.com>
2018-01-18 23:24:41 -0800

    adding dashboard stuff

--- dash_dashboard/app.py
+++ dash_dashboard/app.py
@@ -48,114 +48,27 @@ app.layout = html.Div(children=[
 			dcc.Dropdown(
 				id='first_input', 
 				options=[
-				{'label': 'area_worst', 'value': 'area_worst'},
-				{'label': 'perimeter_worst', 'value': 'perimeter_worst'},
-				{'label': 'concave_points_worst', 'value': 'concave_points_worst'},
-				{'label': 'radius_worst', 'value': 'radius_worst'},
-				{'label': 'concavity_mean', 'value': 'concavity_mean'},
-				{'label': 'area_mean', 'value': 'area_mean'},
-				{'label': 'radius_mean', 'value': 'radius_mean'},
-				{'label': 'perimeter_mean', 'value': 'perimeter_mean'},
-				{'label': 'area_se', 'value': 'area_se'},
-				{'label': 'concavity_worst', 'value': 'concavity_worst'},
-				{'label': 'compactness_worst', 'value': 'compactness_worst'},
-				{'label': 'compactness_mean', 'value': 'compactness_mean'},
-				{'label': 'texture_worst', 'value': 'texture_worst'},
-				{'label': 'radius_se', 'value': 'radius_se'},
-				{'label': 'perimeter_se', 'value': 'perimeter_se'},
-				{'label': 'texture_mean', 'value': 'texture_mean'},
-				{'label': 'symmetry_worst', 'value': 'symmetry_worst'},
-				{'label': 'smoothness_worst', 'value': 'smoothness_worst'},
-				{'label': 'concavity_se', 'value': 'concavity_se'},
-				{'label': 'concave_points_se', 'value': 'concave_points_se'},
-				{'label': 'smoothness_mean', 'value': 'smoothness_mean'},
-				{'label': 'fractal_dimension_se', 'value': 'fractal_dimension_se'},
-				{'label': 'fractal_dimension_worst', 'value': 'fractal_dimension_worst'},
-				{'label': 'fractal_dimension_mean', 'value': 'fractal_dimension_mean'},
-				{'label': 'smoothness_se', 'value': 'smoothness_se'},
-				{'label': 'symmetry_mean', 'value': 'symmetry_mean'},
-				{'label': 'texture_se', 'value': 'texture_se'},
-				{'label': 'symmetry_se', 'value': 'symmetry_se'},
-				{'label': 'compactness_se', 'value': 'compactness_se'}
+				{'label': i, 'value': i} for i in names[2:]
 				], 
 				value = 'area_worst'
 				), 
 			dcc.Dropdown(
 				id='second_input', 
 				options=[
-				{'label': 'area_worst', 'value': 'area_worst'},
-				{'label': 'perimeter_worst', 'value': 'perimeter_worst'},
-				{'label': 'concave_points_worst', 'value': 'concave_points_worst'},
-				{'label': 'radius_worst', 'value': 'radius_worst'},
-				{'label': 'concavity_mean', 'value': 'concavity_mean'},
-				{'label': 'area_mean', 'value': 'area_mean'},
-				{'label': 'radius_mean', 'value': 'radius_mean'},
-				{'label': 'perimeter_mean', 'value': 'perimeter_mean'},
-				{'label': 'area_se', 'value': 'area_se'},
-				{'label': 'concavity_worst', 'value': 'concavity_worst'},
-				{'label': 'compactness_worst', 'value': 'compactness_worst'},
-				{'label': 'compactness_mean', 'value': 'compactness_mean'},
-				{'label': 'texture_worst', 'value': 'texture_worst'},
-				{'label': 'radius_se', 'value': 'radius_se'},
-				{'label': 'perimeter_se', 'value': 'perimeter_se'},
-				{'label': 'texture_mean', 'value': 'texture_mean'},
-				{'label': 'symmetry_worst', 'value': 'symmetry_worst'},
-				{'label': 'smoothness_worst', 'value': 'smoothness_worst'},
-				{'label': 'concavity_se', 'value': 'concavity_se'},
-				{'label': 'concave_points_se', 'value': 'concave_points_se'},
-				{'label': 'smoothness_mean', 'value': 'smoothness_mean'},
-				{'label': 'fractal_dimension_se', 'value': 'fractal_dimension_se'},
-				{'label': 'fractal_dimension_worst', 'value': 'fractal_dimension_worst'},
-				{'label': 'fractal_dimension_mean', 'value': 'fractal_dimension_mean'},
-				{'label': 'smoothness_se', 'value': 'smoothness_se'},
-				{'label': 'symmetry_mean', 'value': 'symmetry_mean'},
-				{'label': 'texture_se', 'value': 'texture_se'},
-				{'label': 'symmetry_se', 'value': 'symmetry_se'},
-				{'label': 'compactness_se', 'value': 'compactness_se'}
+				{'label': i, 'value': i} for i in names[2:]
 				], 
 				value = 'perimeter_worst'
 				),
 			dcc.Dropdown(
 				id='third_input', 
 				options=[
-				{'label': 'area_worst', 'value': 'area_worst'},
-				{'label': 'perimeter_worst', 'value': 'perimeter_worst'},
-				{'label': 'concave_points_worst', 'value': 'concave_points_worst'},
-				{'label': 'radius_worst', 'value': 'radius_worst'},
-				{'label': 'concavity_mean', 'value': 'concavity_mean'},
-				{'label': 'area_mean', 'value': 'area_mean'},
-				{'label': 'radius_mean', 'value': 'radius_mean'},
-				{'label': 'perimeter_mean', 'value': 'perimeter_mean'},
-				{'label': 'area_se', 'value': 'area_se'},
-				{'label': 'concavity_worst', 'value': 'concavity_worst'},
-				{'label': 'compactness_worst', 'value': 'compactness_worst'},
-				{'label': 'compactness_mean', 'value': 'compactness_mean'},
-				{'label': 'texture_worst', 'value': 'texture_worst'},
-				{'label': 'radius_se', 'value': 'radius_se'},
-				{'label': 'perimeter_se', 'value': 'perimeter_se'},
-				{'label': 'texture_mean', 'value': 'texture_mean'},
-				{'label': 'symmetry_worst', 'value': 'symmetry_worst'},
-				{'label': 'smoothness_worst', 'value': 'smoothness_worst'},
-				{'label': 'concavity_se', 'value': 'concavity_se'},
-				{'label': 'concave_points_se', 'value': 'concave_points_se'},
-				{'label': 'smoothness_mean', 'value': 'smoothness_mean'},
-				{'label': 'fractal_dimension_se', 'value': 'fractal_dimension_se'},
-				{'label': 'fractal_dimension_worst', 'value': 'fractal_dimension_worst'},
-				{'label': 'fractal_dimension_mean', 'value': 'fractal_dimension_mean'},
-				{'label': 'smoothness_se', 'value': 'smoothness_se'},
-				{'label': 'symmetry_mean', 'value': 'symmetry_mean'},
-				{'label': 'texture_se', 'value': 'texture_se'},
-				{'label': 'symmetry_se', 'value': 'symmetry_se'},
-				{'label': 'compactness_se', 'value': 'compactness_se'}
+				{'label': i, 'value': i} for i in names[2:]
 				], 
 				value = 'concave_points_worst'
 				)],
 		style={'width': '25%', 'display': 'inline-block'}),
-	html.Div([
-		dcc.Graph(
+	dcc.Graph(
 			id='scatter_plot_3d' 
-			)],
-			style={'width': '75%', 'display': 'inline-block'} 
 		)
 	])
 
@@ -170,19 +83,36 @@ def update_figure(first_input_name, second_input_name, third_input_name):
 	traces = []
 	for i in breast_cancer.diagnosis.unique():
 		breast_cancer_dx = breast_cancer[breast_cancer['diagnosis'] == i]
-		traces.append(go.Scatter3d(
-			x=breast_cancer_dx[first_input_name],
-			y=breast_cancer_dx[second_input_name],
-			z=breast_cancer_dx[third_input_name],
-			text=breast_cancer_dx['diagnosis'],
-			mode='markers',
-			opacity=0.7,
-			marker={
-			'size': 15,
-			'line': {'width': 0.5, 'color': 'white'}
-			},
-			name=i
-	))	
+		if (i == 0):
+			traces.append(go.Scatter3d(
+				x=breast_cancer_dx[first_input_name],
+				y=breast_cancer_dx[second_input_name],
+				z=breast_cancer_dx[third_input_name],
+				text=breast_cancer_dx['diagnosis'],
+				mode='markers',
+				opacity=0.7,
+				marker={
+				'size': 15,
+				'line': {'width': 0.5, 'color': 'white'},
+				'color': 'red'
+				},
+				name='Malignant'
+		))
+		else: 
+			traces.append(go.Scatter3d(
+				x=breast_cancer_dx[first_input_name],
+				y=breast_cancer_dx[second_input_name],
+				z=breast_cancer_dx[third_input_name],
+				text=breast_cancer_dx['diagnosis'],
+				mode='markers',
+				opacity=0.7,
+				marker={
+				'size': 15,
+				'line': {'width': 0.5, 'color': 'white'},
+				'color': '#875FDB'
+				},
+				name='Benign'
+				))
 	return {
 	'data': traces, 
 	'layout': go.Layout(
@@ -190,8 +120,7 @@ def update_figure(first_input_name, second_input_name, third_input_name):
 		yaxis={'title': second_input_name},
 		margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
 		legend={'x': 0, 'y': 1},
-		hovermode='closest',
-		width = 1000,
+		hovermode='closest'
 		)
 	}
 
